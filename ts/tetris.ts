964715df3fa0de50882456f387087225b6b5e7a1
Peter Myers <peter@pcmyers.com>
2023-04-16 23:51:11 -0400

    Initial port to typescript

---
+++ ts/tetris.ts
@@ -0,0 +1,18 @@
+interface Point {
+    x: number;
+    y: number;
+}
+
+interface Tetromino {
+    shape: number[][];
+    position: Point;
+}
+
+class Game {
+    // Implement game logic, including methods for moving, rotating, and managing tetrominoes
+}
+
+class Renderer {
+    // Implement rendering logic, including drawing tetrominoes on the canvas
+}
+
