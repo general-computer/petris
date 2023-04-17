713c973bd4bb5eff594508b6a591515a3bade724
Peter Myers <peter@pcmyers.com>
2023-04-16 23:55:04 -0400

    Expand renderer

--- ts/tetris.ts
+++ ts/tetris.ts
@@ -25,12 +25,67 @@ class Game {
 }
 
 class Renderer {
-    // Add properties, such as canvas, context, etc.
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+    private game: Game;
+    private scale: number;
+
+    constructor(canvas: HTMLCanvasElement, game: Game, scale: number = 20) {
+        this.canvas = canvas;
+        this.context = this.canvas.getContext('2d');
+        this.game = game;
+        this.scale = scale;
+    }
 
     draw(): void {
-        // Implement drawing logic, including drawing the grid and the current tetromino
+        // Clear the canvas
+        this.context.fillStyle = 'white';
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // Draw the grid
+        for (let row = 0; row < this.game.grid.length; row++) {
+            for (let col = 0; col < this.game.grid[row].length; col++) {
+                const cell = this.game.grid[row][col];
+                if (cell) {
+                    this.drawCell(col, row, cell);
+                }
+            }
+        }
+
+        // Draw the current tetromino
+        const tetromino = this.game.currentTetromino;
+        const shape = tetromino.shape;
+
+        for (let row = 0; row < shape.length; row++) {
+            for (let col = 0; col < shape[row].length; col++) {
+                const cell = shape[row][col];
+                if (cell) {
+                    const x = tetromino.position.x + col;
+                    const y = tetromino.position.y + row;
+                    this.drawCell(x, y, cell);
+                }
+            }
+        }
+    }
+
+    private drawCell(x: number, y: number, color: string): void {
+        this.context.fillStyle = color;
+        this.context.fillRect(
+            x * this.scale,
+            y * this.scale,
+            this.scale,
+            this.scale
+        );
+        this.context.strokeStyle = 'white';
+        this.context.strokeRect(
+            x * this.scale,
+            y * this.scale,
+            this.scale,
+            this.scale
+        );
     }
 }
+
 const game = new Game();
 const renderer = new Renderer(/* Pass the canvas element */);
 
